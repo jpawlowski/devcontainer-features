d875e2e432aca6e81d19a93f6b571fb21bbc6c3c
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-07-26 17:14:54 +0000

    refactoring tests

--- test/powershell-extended/install_powershell_profile.sh
+++ test/powershell-extended/install_powershell_profile.sh
@@ -6,7 +6,14 @@ set -e
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
 
+get_default_shell() {
+    local user="$1"
+    getent passwd "$user" | cut -d: -f7
+}
+
 # Extension-specific tests
+check "default shell (user)" get_default_shell "$(whoami)" | grep -q "pwsh$" && exit 0 || exit 1
+check "default shell (root)" get_default_shell root | grep -q "pwsh$" && exit 0 || exit 1
 check "profile" pwsh -Command "if (\$null -eq \$env:ProfileLoaded) { echo 'Not set!'; exit 1 } else { if ( [bool]\$env:ProfileLoaded ) { echo 'Profile loaded.'; exit 0 } else { echo 'False value!'; exit 1 } }"
 
 # Report result
